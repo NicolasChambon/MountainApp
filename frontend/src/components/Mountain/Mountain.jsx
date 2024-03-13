f7260ea3f3666e4b62a6435cbdb0c1082c59c02f
NicolasChambon <nico.chambon@gmail.com>
2024-03-13 22:04:15 +0100

    retour en arrière et suppression de l'utilisation de l'api google map

--- frontend/src/components/Mountain/Mountain.jsx
+++ frontend/src/components/Mountain/Mountain.jsx
@@ -1,7 +1,4 @@
 import PropTypes from "prop-types";
-import GoogleMapReact from "google-map-react";
-
-import MapPin from "../MapPin/MapPin";
 
 import "./Mountain.scss";
 
@@ -18,22 +15,6 @@ function Mountain({ imgLink, title, region, country }) {
       <p className="Mountain-name">{title}</p>
       <p className="Mountain-region">Region : {region}</p>
       <p className="Mountain-country">Country : {country}</p>
-      <div
-        className="Mountain-google-map-container"
-        style={{ height: "15rem" }}
-      >
-        <GoogleMapReact
-          className="Mountain-google-map-container-map"
-          bootstrapURLKeys={{ key: "" }}
-          defaultCenter={{
-            lat: 45.8325,
-            lng: 6.8651,
-          }}
-          defaultZoom={11}
-        >
-          <MapPin lat={45.8325} lng={6.8651} text="Mon Blanc" />
-        </GoogleMapReact>
-      </div>
     </div>
   );
 }
