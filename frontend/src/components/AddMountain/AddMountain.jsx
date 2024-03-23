ff8c99c82e1f585cf95277b690ea09c0dfbafbbd
NicolasChambon <nico.chambon@gmail.com>
2024-03-23 23:54:53 +0100

    externalisation of an EditMap component from AddMountain

--- frontend/src/components/AddMountain/AddMountain.jsx
+++ frontend/src/components/AddMountain/AddMountain.jsx
@@ -1,17 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { MapContainer, TileLayer, useMapEvent } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import { FaMountain } from "react-icons/fa6";
 
 import "./AddMountain.scss";
-import "leaflet/dist/leaflet.css";
-import { addMountain } from "../../actions/mountainActions";
 
-function HandleMapClick() {
-  useMapEvent("click", (e) => {
-    console.log(e.latlng);
-  });
-}
+import { EditMap } from "../EditMap/EditMap";
+
+import { addMountain } from "../../actions/mountainActions";
 
 const AddMountain = () => {
   const regions = useSelector((state) => state.region.regions);
@@ -101,20 +97,7 @@ const AddMountain = () => {
         className="AddMountain-input"
         required
       />
-      <MapContainer
-        center={[10, 10]}
-        zoom={1}
-        style={{ height: "400px", width: "50%", margin: "0 0 1rem 0" }}
-        onClick={(e) => {
-          console.log(e.latlng);
-        }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <HandleMapClick />
-      </MapContainer>
+      <EditMap />
       <input
         type="number"
         id="altitude"
---
+++ frontend/src/components/EditMap/EditMap.jsx
@@ -0,0 +1,28 @@
+import { MapContainer, TileLayer, useMapEvent } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
+function HandleMapClick() {
+  useMapEvent("click", (e) => {
+    console.log(e.latlng);
+  });
+}
+
+export const EditMap = () => {
+  return (
+    <MapContainer
+      center={[10, 10]}
+      zoom={1}
+      style={{ height: "400px", width: "50%", margin: "0 0 1rem 0" }}
+      onClick={(e) => {
+        console.log(e.latlng);
+      }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <HandleMapClick />
+    </MapContainer>
+  );
+};
